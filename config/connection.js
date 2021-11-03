1953d07a4c15c8e5188d7a0a6ce3b3515ab94b79
JBanana <johnandrewharris3@gmail.com>
2021-11-03 17:50:37 -0500

    stuff

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,20 @@ const Sequelize = require("sequelize");
 //executes when we use connection.js to make data in .env file available at process.env.
 require("dotenv").config();
 
+
 //create connection to our database, pass in your MySQL information for username and password 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306
-});
+//when the app is deployed it will have access to herokus variable, otherwise it will continue using localhost congifuration
+let sequelize;
+
+if(process.env.JAWSDB_URL){
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+else{
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: "localhost",
+        dialect: "mysql",
+        port: 3306
+    });
+}
 
 module.exports = sequelize
