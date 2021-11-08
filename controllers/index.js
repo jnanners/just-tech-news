79f8153328536810dd68ead8542a34e576087a1e
JBanana <johnandrewharris3@gmail.com>
2021-11-07 20:42:29 -0600

    started

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
 
+router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 
 //if a request is made to an endpoint that doesn't exist a 404 error will be returned
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const path = require("path");
+//handlebars setup
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001
 
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+//handlebars setup
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+//make files in public folder available to client
+app.use(express.static(path.join(__dirname, "public")));
 
 //turn on routes
 app.use(routes);
