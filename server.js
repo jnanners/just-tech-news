9ec74c2dd8eb36f97f016e23497382aa2c9c57c7
JBanana <johnandrewharris3@gmail.com>
2021-11-08 18:13:16 -0600

    something doesn't work lol

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
+const session = require("express-session");
+
 //handlebars setup
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
@@ -9,9 +11,23 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001
 
+const sequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+    secret: "Super secret secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new sequelizeStore({
+        db: sequelize
+    })
+};
+
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(session(sess));
 
 //handlebars setup
 app.engine("handlebars", hbs.engine);
