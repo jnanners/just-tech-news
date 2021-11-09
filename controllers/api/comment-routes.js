e882af4b5c7653039b707929fe30bbc5403f8a48
JBanana <johnandrewharris3@gmail.com>
2021-11-09 13:24:11 -0600

    3 done

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,48 +1,49 @@
-const router = require("express").Router();
-const {Comment} = require("../../models");
+const router = require('express').Router();
+const { Comment } = require('../../models');
 
-//get comments
-router.get("/", (req, res) => {
-    Comment.findAll({})
-    .then(result => res.json(result))
+router.get('/', (req, res) => {
+  Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
-        console.log(err);
-        res.json(err);
-    })
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-//post comment
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
+  // check the session
+  if (req.session) {
     Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id
     })
-    .then(result => res.json(result))
-    .catch(err => {
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
         console.log(err);
         res.status(400).json(err);
-    })
+      });
+  }
 });
 
-//delete comment
-router.delete("/:id", (req, res) => {
-    Comment.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(result => {
-        if(!result){
-            res.status(404).json({message: "No commend found with this id"});
-            return;
-        }
-        res.json(result);
+router.delete('/:id', (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCommentData => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'No comment found with this id!' });
+        return;
+      }
+      res.json(dbCommentData);
     })
     .catch(err => {
-        console.log(err);
-        res.json(err);
-    })
-})
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,9 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const apiRoutes = require("./api");
-const homeRoutes = require("./home-routes");
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
 
-router.use("/", homeRoutes);
-router.use("/api", apiRoutes);
-
-//if a request is made to an endpoint that doesn't exist a 404 error will be returned
-router.use((req, res) => {
-    res.status(404).end();
-})
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
